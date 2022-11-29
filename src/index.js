c84c20f4c09187c9f1da74b24e8af749932c86e2
HaileyJeong <115683734+HaileyJeong@users.noreply.github.com>
2022-11-30 00:50:16 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const gameForm = document.getElementById("game-form");
+const rangeNum = document.getElementById("rangevalue");
+const selectNum = document.getElementById("selectvalue");
+const gameSummary = document.getElementById("summary");
+const gameResult = document.getElementById("result");
+
+function makeRandNum() {
+  return Math.ceil(Math.random() * rangeNum.value);
+}
+
+function handleGameResult(event) {
+  event.preventDefault();
+
+  const userNum = Number(selectNum.value);
+  const machineNum = makeRandNum(rangeNum);
+  gameSummary.innerText = `You chose: ${userNum}, the machine chose ${machineNum}.`;
+
+  if (userNum === machineNum) {
+    gameResult.innerText = "You win!";
+  } else {
+    gameResult.innerText = "You lose!";
+  }
+}
+
+gameForm.addEventListener("submit", handleGameResult);
